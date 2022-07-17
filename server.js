f037c5fa06d3322a463f7119acc5c572cbf6bdbb
ibraphem <ibraphem@gmail.com>
2022-07-17 10:39:44 +0100

    mongodb and mongoose setup

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 import express from "express"
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
 
 const app = express();
+
+dotenv.config()
+mongoose.connect(process.env.MONGODB_URI).then(() => console.log("connected to DB")).catch((err) => console.log(err.message))
 
 const port = process.env.PORT || 7000
 app.listen(port, () => {
